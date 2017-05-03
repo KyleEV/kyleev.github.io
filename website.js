9fd74ae462c6821122371fcba60b946c179772ca
KyleEV <itz_kylee@live.co.uk>
2017-05-03 16:31:43 +0100

    java
    k

---
+++ website.js
@@ -0,0 +1,16 @@
+function homeImage() {
+document.getElementById("Home").innerHTML= "<img src='Home.png' width='75' />";
+
+}
+function aboutImage() {
+document.getElementById("About").innerHTML= "<img src='About.png' width='75' />";
+
+}
+function workImage() {
+document.getElementById("Work").innerHTML= "<img src='Work.png' width='75' />";
+
+}
+function contactImage() {
+document.getElementById("Contact").innerHTML= "<img src='Contact.png' width='75' />";
+
+}

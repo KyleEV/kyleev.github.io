1dd0a3d77c90dd0e54d917fdc76749ab9c062832
KyleEV <itz_kylee@live.co.uk>
2017-05-17 17:51:30 +0100

    qwdqw

--- website.js
+++ website.js
@@ -13,4 +13,20 @@ document.getElementById("Work").innerHTML= "<img src='Work.png' width='75' />";
 function contactImage() {
 document.getElementById("Contact").innerHTML= "<img src='Contact.png' width='75' />";
 
+function homeAway() {
+document.getElementById("Home").innerHTML= "<img src='Home.png' width='75' />";
+
+}
+function aboutAway() {
+document.getElementById("About").innerHTML= "<img src='About.png' width='75' />";
+
+}
+function workAway() {
+document.getElementById("Work").innerHTML= "<img src='Work.png' width='75' />";
+
+}
+function contactAway() {
+document.getElementById("Contact").innerHTML= "<img src='Contact.png' width='75' />";
+
+
 }

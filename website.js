8838c0fcdbdf35ab4ceb8cf92676c4b7387e11c1
KyleEV <itz_kylee@live.co.uk>
2017-05-17 18:02:29 +0100

    sadf

--- website.js
+++ website.js
@@ -1,32 +1,10 @@
 function homeImage() {
-document.getElementById("Home").innerHTML= "<img src='Home.png' width='75' />";
+document.getElementById("h1").style.color = "red";
 
-}
-function aboutImage() {
-document.getElementById("About").innerHTML= "<img src='About.png' width='75' />";
 
-}
-function workImage() {
-document.getElementById("Work").innerHTML= "<img src='Work.png' width='75' />";
-
-}
-function contactImage() {
-document.getElementById("Contact").innerHTML= "<img src='Contact.png' width='75' />";
 
 function homeAway() {
 document.getElementById("Home").innerHTML= "<img src='Home.png' width='75' />";
-
-}
-function aboutAway() {
-document.getElementById("About").innerHTML= "<img src='About.png' width='75' />";
-
-}
-function workAway() {
-document.getElementById("Work").innerHTML= "<img src='Work.png' width='75' />";
-
-}
-function contactAway() {
-document.getElementById("Contact").innerHTML= "<img src='Contact.png' width='75' />";
 
 
 }

871947429074676bef137553ed00ac87eadcdce7
KyleEV <itz_kylee@live.co.uk>
2017-05-17 18:20:50 +0100

    fgh

--- website.js
+++ website.js
@@ -1,10 +1,3 @@
 function homeImage() {
 document.getElementById("Home").innerHTML= "<img src='download.jpg>";
-
-
-
-function homeAway() {
-document.getElementById("Home").innerHTML= "<img src='Home.png' width='75' />";
-
-
 }

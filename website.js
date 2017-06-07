789dfe6c1c8560adbcc1d5f25730573aaf8176b3
KyleEV <itz_kylee@live.co.uk>
2017-06-07 10:59:11 +0100

    tyu

--- website.js
+++ website.js
@@ -3,3 +3,7 @@ function homeImage() {
 	console.log ("rollingover");
 document.getElementById("piano").src='download.jpg';
 }
+function homeOut() {
+ console.log ("rollingover");
+ document.getElementById ("piano").src='piano.png';
+}
